a73e7e6f68bc2eb4f8578d12fcbaa3c11a74eda6
ShishirBhandari <shishirbhandari54@gmail.com>
2019-10-03 19:51:32 +0545

    assignment 2 in progress

--- assignment-1/js/carousel.js
+++ assignment-1/js/carousel.js
@@ -1,38 +1,38 @@
-var carousel = document.getElementsByClassName("carousel-container")[0];
-var wrapper = document.getElementsByClassName("carousel-image-wrapper")[0];
+var carousel = document.getElementsByClassName('carousel-container')[0];
+var wrapper = document.getElementsByClassName('carousel-image-wrapper')[0];
 var currentImageIndex = 0;
 
 // Styles for carousel-image-wrapper
-wrapper.style.width = 552 * 6 + "" + "px";
-wrapper.style.float = "left";
-wrapper.style.marginLeft = "0px";
+wrapper.style.width = 552 * 6 + '' + 'px';
+wrapper.style.float = 'left';
+wrapper.style.marginLeft = '0px';
 
 // Get images
 images = document.querySelectorAll(
-  ".carousel-container .carousel-image-wrapper img"
+  '.carousel-container .carousel-image-wrapper img'
 );
 var imagesCount = images.length;
 
 //Create button for arrows
 function createButton() {
-  var button = document.createElement("button");
-  button.style.position = "absolute";
-  button.style.top = "50%";
-  button.style.width = "14px";
-  button.style.height = "22px";
-  button.style.transform = "translate(0, -50%)";
-  button.style.border = "none";
+  var button = document.createElement('button');
+  button.style.position = 'absolute';
+  button.style.top = '50%';
+  button.style.width = '14px';
+  button.style.height = '22px';
+  button.style.transform = 'translate(0, -50%)';
+  button.style.border = 'none';
   return button;
 }
 
 // Left arrow button
 var leftArrow = createButton();
-leftArrow.style.background = "url(./images/left-arrow.png) no-repeat";
+leftArrow.style.background = 'url(./images/left-arrow.png) no-repeat';
 leftArrow.style.left = 0;
 
 //right arrow button
 var rightArrow = createButton();
-rightArrow.style.background = "url(./images/right-arrow.png) no-repeat";
+rightArrow.style.background = 'url(./images/right-arrow.png) no-repeat';
 rightArrow.style.right = 0;
 
 // On click right arrow
@@ -74,7 +74,7 @@ function animateWrapper(direction, value) {
   setTimeout(function() {
     stopAnimation = true;
     wrapper.style.marginLeft =
-      parseInt(previousMargin) + direction * value + "px";
+      parseInt(previousMargin) + direction * value + 'px';
 
     disableButtons(false);
     setupIndicators();
@@ -85,17 +85,17 @@ function animateWrapper(direction, value) {
     if (!stopAnimation && runningPos <= value) {
       runningPos += offset;
       wrapper.style.marginLeft =
-        parseInt(wrapper.style.marginLeft) + direction * offset + "px";
+        parseInt(wrapper.style.marginLeft) + direction * offset + 'px';
     }
   }, 18);
 }
 
 // Styles for Carousel-container
-carousel.style.position = "relative";
-carousel.style.padding = "20px 20px";
-carousel.style.width = "512px";
-carousel.style.overflow = "hidden";
-carousel.setAttribute("class", carousel.getAttribute("class") + " clearfix");
+carousel.style.position = 'relative';
+carousel.style.padding = '20px 20px';
+carousel.style.width = '512px';
+carousel.style.overflow = 'hidden';
+carousel.setAttribute('class', carousel.getAttribute('class') + ' clearfix');
 
 carousel.appendChild(leftArrow);
 carousel.appendChild(rightArrow);
@@ -103,11 +103,11 @@ carousel.appendChild(rightArrow);
 // Applying styles for each image
 for (var i = 0; i < images.length; i++) {
   const img = images[i];
-  img.style.float = "left";
-  img.style.margin = "0 20px";
+  img.style.float = 'left';
+  img.style.margin = '0 20px';
 }
-images[0].style.marginLeft = "0px";
-images[images.length - 1].style.marginRight = "0px";
+images[0].style.marginLeft = '0px';
+images[images.length - 1].style.marginRight = '0px';
 
 //
 //
@@ -116,33 +116,33 @@ images[images.length - 1].style.marginRight = "0px";
 var indicators;
 
 // create indicators container
-var indicatorsContainer = document.createElement("ul");
+var indicatorsContainer = document.createElement('ul');
 
 // create indicator button for each image
 for (var i = 0; i < imagesCount; i++) {
-  var indicator = document.createElement("li");
-  var button = document.createElement("button");
-  button.innerHTML = "o";
-  button.style.padding = "5px";
-  button.style.margin = "5px";
-  button.style.border = "none";
-  button.setAttribute("class", "indicator");
+  var indicator = document.createElement('li');
+  var button = document.createElement('button');
+  button.innerHTML = 'o';
+  button.style.padding = '5px';
+  button.style.margin = '5px';
+  button.style.border = 'none';
+  button.setAttribute('class', 'indicator');
 
   indicator.appendChild(button);
 
-  indicator.style.listStyle = "none";
-  indicator.style.float = "left";
+  indicator.style.listStyle = 'none';
+  indicator.style.float = 'left';
 
   indicatorsContainer.appendChild(indicator);
 }
 
-indicatorsContainer.style.display = "inline-block";
+indicatorsContainer.style.display = 'inline-block';
 
 // added indicators to the carousel container
 carousel.appendChild(indicatorsContainer);
 
 // Select and get indicator buttons
-var buttons = document.querySelectorAll(".carousel-container button.indicator");
+var buttons = document.querySelectorAll('.carousel-container button.indicator');
 
 // Styling indicators according to slide position
 function setupIndicators() {
@@ -150,9 +150,9 @@ function setupIndicators() {
     const element = buttons[i];
 
     if (i == currentImageIndex) {
-      buttons[i].style.backgroundColor = "#0a0";
+      buttons[i].style.backgroundColor = '#0a0';
     } else {
-      buttons[i].style.backgroundColor = "#aaa";
+      buttons[i].style.backgroundColor = '#aaa';
     }
 
     // Indicators clickable functionality
